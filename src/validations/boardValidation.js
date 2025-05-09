9e3eeb67231b50552a02e7cfe4b7572ad1f2a848
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-05-09 14:37:58 +0700

    Error handing middleware

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   const correctValidation = Joi.object({
@@ -13,9 +14,7 @@ const createNew = async (req, res, next) => {
     // validate dữ liệu thành công next qua controller
     next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 
